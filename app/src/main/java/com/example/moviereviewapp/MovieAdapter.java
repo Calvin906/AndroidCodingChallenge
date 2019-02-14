6b121679d8d570e654e702d13840ae1983165643
Alex Preston <jacobpreston4@Jacobs-MBP.attlocal.net>
2019-02-13 22:27:54 -0800

    Added date converter
    Added minWidth to handle weird loading problems

--- app/src/main/java/com/example/moviereviewapp/MovieAdapter.java
+++ app/src/main/java/com/example/moviereviewapp/MovieAdapter.java
@@ -2,6 +2,7 @@ package com.example.moviereviewapp;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +12,12 @@ import android.widget.TextView;
 import com.example.moviereviewapp.model.Movie;
 import com.squareup.picasso.Picasso;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -46,7 +51,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
 
     @Override
     public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
-        if (position == getItemCount()-1) {
+        if (position == getItemCount() - 1) {
             listener.onRequestLoadMore(position);
         }
         MovieViewHolder dataHolder = holder;
@@ -59,20 +64,32 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
     }
 
     static class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        @BindView(R.id.title_view) TextView title;
-        @BindView(R.id.mpaa_rating_view) TextView rating;
-        @BindView(R.id.critic_pick_view) TextView criticPick;
-        @BindView(R.id.byline_view) TextView byline;
-        @BindView(R.id.headline_view) TextView headline;
-        @BindView(R.id.summary_view) TextView summary;
-        @BindView(R.id.publication_date_view) TextView publicationDate;
-        @BindView(R.id.opening_date_view) TextView openingDate;
-        @BindView(R.id.date_updated_view) TextView dateUpdated;
-        @BindView(R.id.thumbnail_image) ImageView thumbnail;
+        @BindView(R.id.title_view)
+        TextView title;
+        @BindView(R.id.mpaa_rating_view)
+        TextView rating;
+        @BindView(R.id.critic_pick_view)
+        TextView criticPick;
+        @BindView(R.id.byline_view)
+        TextView byline;
+        @BindView(R.id.headline_view)
+        TextView headline;
+        @BindView(R.id.summary_view)
+        TextView summary;
+        @BindView(R.id.publication_date_view)
+        TextView publicationDate;
+        @BindView(R.id.opening_date_view)
+        TextView openingDate;
+        @BindView(R.id.date_updated_view)
+        TextView dateUpdated;
+        @BindView(R.id.thumbnail_image)
+        ImageView thumbnail;
 
         private final MovieAdapterListener listener;
         private Movie movie;
-
+        private static final String TAG = "VIEW_HOLDER";
+        private final SimpleDateFormat oldFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        private final SimpleDateFormat newFormatter = new SimpleDateFormat("MM/dd/yyyy - hh:mm a");
 
         MovieViewHolder(View v, MovieAdapterListener listener) {
             super(v);
@@ -91,8 +108,25 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
             summary.setText(movie.getSummary());
             publicationDate.setText(movie.getPublicationDate());
             openingDate.setText(movie.getOpeningDate());
-            dateUpdated.setText(movie.getDateUpdated());
+            dateUpdated.setText(convertToLocal(movie.getDateUpdated()));
             Picasso.with(itemView.getContext()).load(movie.getThumbnail().getSrc()).into(thumbnail);
+        }
+
+        private String convertToLocal(String date) {
+
+            oldFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+            Date value = null;
+            String localDate = "";
+            try {
+                value = oldFormat.parse(date);
+
+                newFormatter.setTimeZone(TimeZone.getDefault());
+                localDate = newFormatter.format(value);
+            } catch (ParseException e) {
+                Log.e(TAG, e.getMessage());
+            }
+
+            return localDate;
         }
 
 
